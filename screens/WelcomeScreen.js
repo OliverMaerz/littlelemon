af9c17eac731478e29210dc4adf4ea3880240178
Oliver Maerz <om@berlinco.com>
2024-02-03 17:40:28 -0300

    clean up code

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Text, View, Image, Pressable} from 'react-native'; // Import Pressable
+import {Text, View, Image, Pressable} from 'react-native';
 
 /**
  * Welcome screen component
